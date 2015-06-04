770b1e176e8b4bde8463ae262efe06af2b9d9195
Karen Levy <karenlevy123@gmail.com>
2015-06-03 19:48:48 -0700

    add images and bg

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -10,18 +10,57 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+    @IBOutlet var imageSwipe: UIPanGestureRecognizer!
+    var cardInitialCenter: CGPoint!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var nav = self.navigationController?.navigationBar
+        // 2
+        nav?.barStyle = UIBarStyle.Black
+        nav?.tintColor = UIColor.yellowColor()
+        // 3
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        imageView.contentMode = .ScaleAspectFit
+        // 4
+        let image = UIImage(named: "nav_bar.png")
+        imageView.image = image
+        nav?.setBackgroundImage(image, forBarMetrics: .Default)
+
+        // Dispose of any resources that can be recreated.
 
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
 
+    @IBAction func onPanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
+        
+        var point = panGestureRecognizer.locationInView(view)
+        let translation = panGestureRecognizer.translationInView(self.view)
+        var velocity = panGestureRecognizer.velocityInView(view)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            cardInitialCenter = trayView.center
+            
+          } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            
+            imageView.center = CGPoint(x: cardInitialCenter.x, y: cardInitialCenter.y + translation.y)
+            
+       
+          } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            
+            
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
