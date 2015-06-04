ea23826add9328b55eab8a15017c18db6e39d14e
David Kuchar <davidkuchar@gmail.com>
2015-06-03 19:49:08 -0700

    started refactor

---
+++ Tinder/DraggableImageView.swift
@@ -0,0 +1,21 @@
+//
+//  DraggableImageView.swift
+//  Tinder
+//
+//  Created by David Kuchar on 6/3/15.
+//  Copyright (c) 2015 David Kuchar. All rights reserved.
+//
+
+import UIKit
+
+class DraggableImageView: UIImageView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
